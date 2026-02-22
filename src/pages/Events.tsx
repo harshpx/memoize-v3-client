240cf4de82c552fde6a4e257cd43b4d2a8e4cd69
harshpx <harsh.rzf@gmail.com>
2026-02-23 01:28:13 +0530

    minor ui change

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,6 +1,6 @@
 const Events = () => {
 	return (
-		<div className="h-full w-full border flex items-center justify-center">
+		<div className="h-full w-full flex items-center justify-center">
 			Events are coming soon!
 		</div>
 	);
